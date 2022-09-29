905f72005a903816c6c6a2603ee224573fce0cb6
qzar1999 <dipster>
2022-09-30 00:38:28 +1000

    remove comment

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# huiBolshoy=False
-
-# if huiBolshoy:
-#     print("yaemspermouv")
-# else:
-#     print("yaneemspermouv")
 cripCount = 1
-
 for i in range(5):
     print(i)
-    # cripCount = cripCount + 1
