5bfb4fed3fab3464ced6a1e6b3ef0d8f9d379222
qzar1999 <dipster>
2022-09-30 00:27:52 +1000

    hui

---
+++ main.py
@@ -0,0 +1,11 @@
+# huiBolshoy=False
+
+# if huiBolshoy:
+#     print("yaemspermouv")
+# else:
+#     print("yaneemspermouv")
+cripCount = 1
+
+for i in range(5):
+    print(i)
+    # cripCount = cripCount + 1
